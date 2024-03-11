2087e7de042c74177f5092d00eaa2ed9fe3acfcd
fateme nabati <fatemenabati22@gmail.com>
2024-03-11 06:00:16 +0330

    Fixed updateExist error

--- apps/universityـrestaurantـwarehouseـmanagement-app/src/routes/exist/services/updateExist.ts
+++ apps/universityـrestaurantـwarehouseـmanagement-app/src/routes/exist/services/updateExist.ts
@@ -1,11 +1,16 @@
 import { pool } from "../../../db"
 export const updateExist = (req, res) => {
-    const warehouse_id = parseInt(req.params.warehouse_id)
-    const item_id = parseInt(req.params.item_id)
-    const { new_warehouse_id, new_item_id, new_unit, new_amount } = req.body
+
+    let { warehouse_id, item_id, unit, amount } = req.body
+    const new_warehouse_id = warehouse_id
+    const new_item_id = item_id
+    const new_unit = unit
+    const new_amount = amount
+    warehouse_id = parseInt(req.params.warehouse_id)
+    item_id = parseInt(req.params.item_id)
   
     pool.query(
-      'UPDATE warehouse_item SET warehouse_id = $1, item_id = $2, unit = $3, amount = $4 WHERE warehouse_id = $5 AND item_id = $6',
+      'UPDATE exist SET warehouse_id = $1, item_id = $2, unit = $3, amount = $4 WHERE warehouse_id = $5 AND item_id = $6',
       [new_warehouse_id, new_item_id, new_unit, new_amount, warehouse_id, item_id],
       (error, results) => {
         if (error) {
