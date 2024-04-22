aec04455a8aeb5631a68c5c27d95d1fe03201ef5
fateme nabati <fatemenabati22@gmail.com>
2024-04-22 23:38:53 +0430

    Make header responsive

--- apps/restaurant-warehouse-management-fr/src/app/components/HeaderComponent.tsx
+++ apps/restaurant-warehouse-management-fr/src/app/components/HeaderComponent.tsx
@@ -47,11 +47,11 @@ function HeaderComponent(props: HeaderComponentProps) {
                     color={"#33d17a"}
                     stroke={1.5}
                   />
-                  <Text color={"#33d17a"}>restaurant warehouse management</Text>
+                  <Text color={"#33d17a"} visibleFrom="xs">restaurant warehouse management</Text>
         </Group>
 
         <Group>
-          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+          <Group ml={50} gap={5} className={classes.links} visibleFrom="md">
             {items}
           </Group>
           <Autocomplete
@@ -59,7 +59,7 @@ function HeaderComponent(props: HeaderComponentProps) {
             placeholder="Search"
             leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
             data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
-            visibleFrom="xs"
+            visibleFrom="lg"
           />
         </Group>
         <Menu
